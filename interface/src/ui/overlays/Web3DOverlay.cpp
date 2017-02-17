9cdd85d9459b26bb49df90201b3a06a864daa4a2
Dante Ruiz <dante@highfidelity.io>
2017-02-17 00:01:12 +0000

    fixed qml font path issue for desktop and tablet

--- interface/src/ui/overlays/Web3DOverlay.cpp
+++ interface/src/ui/overlays/Web3DOverlay.cpp
@@ -38,8 +38,10 @@
 #include "scripting/AccountScriptingInterface.h"
 #include "scripting/HMDScriptingInterface.h"
 #include <Preferences.h>
+#include <ScriptEngines.h>
 #include "FileDialogHelper.h"
 #include "scripting/WindowScriptingInterface.h"
+
 
 static const float DPI = 30.47f;
 static const float INCHES_TO_METERS = 1.0f / 39.3701f;
@@ -188,7 +190,8 @@ void Web3DOverlay::loadSourceURL() {
             _webSurface->getRootContext()->setContextProperty("Account", AccountScriptingInterface::getInstance());
             _webSurface->getRootContext()->setContextProperty("HMD", DependencyManager::get<HMDScriptingInterface>().data());
             _webSurface->getRootContext()->setContextProperty("fileDialogHelper", new FileDialogHelper());
-            _webSurface->getRootContext()->setContextProperty("Window", DependencyManager::get<WindowScriptingInterface>().data());
+            _webSurface->getRootContext()->setContextProperty("ScriptDiscoveryService", DependencyManager::get<ScriptEngines>().data());
+            _webSurface->getRootContext()->setContextProperty("pathToFonts", "../../../");
             tabletScriptingInterface->setQmlTabletRoot("com.highfidelity.interface.tablet.system", _webSurface->getRootItem(), _webSurface.data());
 
             // Override min fps for tablet UI, for silky smooth scrolling
